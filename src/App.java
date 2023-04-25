dc3e4c58766e299ac7fcc02930c8bd4aefcba5d7
Lima <117579221+Lucas00Lima@users.noreply.github.com>
2023-04-25 18:03:57 -0300

    Feito via cmd git

--- src/App.java
+++ src/App.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String filename = "localdoarquivo";
+        String filename = "locaDoArquivo";
         File file = new File();
         List<Dados> dadosList = file.verificarRegras(filename);
         OpenBank open = new OpenBank();
